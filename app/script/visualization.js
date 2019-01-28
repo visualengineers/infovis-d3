3aa97634559fa8fa3cbc17ef0531b601b05bcd26
MarcAber <mbehrens.htw@gmail.com>
2019-01-28 16:06:41 +0100

    Refacturing and Pie Charts Adjustent

--- app/script/visualization.js
+++ app/script/visualization.js
@@ -3,27 +3,7 @@
  * Visualization module
  */
 var Visualization = function () {
-    var _greeting;
-
-    // return {
-    //     /**
-    //      * Create the visualisation. This is the main application entry point.
-    //      */
-    //     draw: function () {
-    //         var _greeting = '<pre>There are ' + DataProvider.getPreparedData().length + ' regions. ';
-    //         DataProvider.getPreparedData().forEach(function (region) {
-    //             _greeting += region.Region + ' has ' + region.Countries.length + ' countries.\n';
-    //         });
-    //         _greeting += 'By the way, Germany had ' + DataProvider.getValue('Germany', '2016', '21042') + ' percent of obese people in 2016.\n\n'
-    //         _greeting += 'Northern Africa had an average index of ' + DataProvider.getAverageForRegion('Northern Africa', '2016', '21032') + ' for political stability in 2015,\n';
-    //         _greeting += 'while Western Europe had ' + DataProvider.getAverageForRegion('Western Europe', '2016', '21032') + '. ';
-    //         var bestIndex = DataProvider.getMaxValue('21032');
-    //         _greeting += 'The best index was ' + bestIndex.Value + ' in ' + bestIndex.Country + ' in the year ' + bestIndex.Year + '.';
-    //         _greeting += '</pre>';
-    //         document.body.innerHTML = _greeting;
-    //     }
-    // }
-
+   
     return {
         /**
          * Create the visualisation. This is the main application entry point.
@@ -44,8 +24,6 @@ var Visualization = function () {
 
                 var width = window.innerWidth - 150;
                 var height = window.innerHeight - 120;
-                console.log("Width: " + width + ", Height: " + height);
-                var transformString = "translate(" + (-0.7 * width) + "," + (-1.1 * height) + ")scale(2.5)";
 
                 var projection = d3.geoNaturalEarth1()
                     .scale(180)
@@ -60,7 +38,7 @@ var Visualization = function () {
                     .attr("class", "tooltip")
                     .style("opacity", 0);
 
-                // PieChart
+                // PieCharts
                 var pie1 = d3.select('.pie1').select('svg');
                 var pie2 = d3.select('.pie2').select('svg');
 
@@ -86,16 +64,13 @@ var Visualization = function () {
                     });
 
                 // Slider zeichnen
-                var gTime = d3
+                var sliderTimeCast = d3
                     .select('div#slider')
                     .append('svg')
                     .append('g')
                     .attr('transform', 'translate(' + (width - 600) / 5 + ',15)')
                     .call(sliderTime);
 
-                // ENDE SLIDER
-
-                // Einfärben Kontinente
                 function paintMapRegions(code, value, year) {
                     var myScale = d3.scaleLinear()
                         .domain([DataProvider.getMinValueYear(code, year).Value, 0, DataProvider.getMaxValueYear(code, year).Value])
@@ -106,8 +81,6 @@ var Visualization = function () {
                     }
                     return color;
                 }
-                // Ende Einfärben Kontinente
-
 
                 function handleMouseOver(d, i) {
 
@@ -125,7 +98,6 @@ var Visualization = function () {
 
                     var red = '#F23C50';
                     var white = '#FFF';
-                    var darkGreen = '#16494F';
                     var blue = '#4AD9D9';
 
                     var pie = d3.pie()
@@ -134,8 +106,8 @@ var Visualization = function () {
                         });
 
                     var arc = d3.arc()
-                        .innerRadius(10)
-                        .outerRadius(30)
+                        .innerRadius(30)
+                        .outerRadius(60)
 
                     pie1.selectAll('path')
                         .data(pie(dataPie))
@@ -145,8 +117,13 @@ var Visualization = function () {
                         .attr('fill', function (d, i) {
                             return [blue, white][i % 2];
                         })
-                        .attr('transform', 'translate(50, 30)')
+                        .attr('transform', 'translate(100, 75)')
 
+                    pie1.append("text")
+                        .attr("text-anchor", "middle")
+                        .text(sanitaer)
+                        .style("fill", "rgb(255, 207, 75)")
+                        .attr('transform', 'translate(100, 80)')
 
                     pie2.selectAll('path')
                         .data(pie(dataPie2))
@@ -156,19 +133,22 @@ var Visualization = function () {
                         .attr('fill', function (d, i) {
                             return [red, white][i % 2];
                         })
-                        .attr('transform', 'translate(50, 30)');
+                        .attr('transform', 'translate(100, 75)');
+                        
+                    pie2.append("text")
+                        .attr("text-anchor", "middle")
+                        .text(undernourished)
+                        .style("fill", "rgb(255, 207, 75)")
+                        .attr('transform', 'translate(100, 80)')
 
                     // Tooltip Handler
                     tooltip.transition()
                         .duration(200)
                         .style("opacity", .8);
-                    tooltip.html(d.properties.name)
+                    tooltip.html(name + "</br>Index: " + DataProvider.getValue(d.properties.iso_a3, year, '21032'))
                         .style("left", (d3.event.pageX) + "px")
                         .style("top", (d3.event.pageY - 28) + "px");
 
-
-                    return document.getElementById('name').innerHTML = "<h3>" + name + "</h3><h4>" + id + "</h4>" + "Sanitary:</br>" + DataProvider.getValue(d.properties.iso_a3, year, '21046') + " %</br>"
-                        + "</br>Undernourished:</br>" + DataProvider.getValue(d.properties.iso_a3, year, '210041') + " %</h5>";
                 }
 
                 // Hintergrund
@@ -204,7 +184,9 @@ var Visualization = function () {
                             .duration(500)
                             .style("opacity", 0);
                         pie1.selectAll("path").remove();
+                        pie1.select('text').remove();
                         pie2.selectAll("path").remove();
+                        pie2.select('text').remove();
                     })
                     ;
             })
